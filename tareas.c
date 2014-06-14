0cb0a68c574232e79673ff7d7ae3d911b7bee4c2
fmonpelat <facundo.monpelat@gmail.com>
2014-06-14 19:46:05 -0300

    tareas

--- tareas.c
+++ tareas.c
@@ -2,17 +2,12 @@
 //  tareas.c
 //  TP4 Algoritmos 1
 //
-<<<<<<< HEAD
 //  Copyright (c) 2014 . All rights reserved.
 //
 //  Facundo Monpelat
 //  Luca Mazzer
 //  Erik Schatz
 
-=======
-//  Facundo Monpelat
-//  Luca Mazzer
-//  Erik Schatz
->>>>>>> FETCH_HEAD
+
 
 #include <stdio.h>

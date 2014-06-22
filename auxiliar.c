89ba8c25dcf7ff23c2fffc3a08290c3b9acfad6b
irekuervo <erik.schatz@gmail.com>
2014-06-22 19:01:44 -0300

    Agregado el validador
    Agregado el validador de de la lectura de partidos.txt

--- auxiliar.c
+++ auxiliar.c
@@ -3,3 +3,45 @@
 #include <string.h>
 #include "tareas.h"
 
+size_t ValidarPartidos(tlista lista)
+{
+	int validado = 1,fila=0,filaAux=0;
+
+	tnodo* nodo = lista;	
+	while (nodo&&validado)
+	{
+		lista = nodo->sig;
+		//brasil alemania alemania brasil
+		while (lista&&validado)
+		{
+			filaAux = 0;
+			if (nodo->dato->idPartido == lista->dato->idPartido)
+				validado = 0;
+			char*nodoEquipo1=nodo->dato->equipo1->id;
+			char*nodoEquipo2 = nodo->dato->equipo2->id;
+			char*listaEquipo1 = lista->dato->equipo1->id;
+			char*listaEquipo2 = lista->dato->equipo2->id;
+
+			if (nodoEquipo1&&nodoEquipo2&&listaEquipo1&&listaEquipo2)
+			{
+				if (!(strcmp(nodoEquipo1, listaEquipo1) && !strcmp(nodoEquipo2, listaEquipo2)) || !(strcmp(nodoEquipo1, listaEquipo2) && !strcmp(nodoEquipo2, listaEquipo1)))
+					validado = 0;
+			}
+			filaAux++;
+			lista = lista->sig;
+		}
+		
+		nodo = nodo->sig;
+		fila++;
+	}
+
+	if (validado)
+		return 1;
+	else
+	{
+		printf("Error en la fila numero: %d \n", fila+filaAux);
+		return 0;
+	}
+		
+
+}
